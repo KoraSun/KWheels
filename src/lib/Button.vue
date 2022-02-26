276efc3d613a8af553eefb35696deeda61b7468f
kora <bettm9706@gmail.com>
2022-02-26 15:45:36 +0800

    button基础css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -11,7 +11,7 @@
         props:{
             theme:{
                 type:String,
-                default:'button'
+                default:"button"
             }
         }
          
@@ -19,5 +19,41 @@
 </script>
 
 <style lang="scss" scoped>
+$h:32px;
+$border-color:#d9d9d9;
+$color:#333;
+$blue:#40a9ff;
+$radius:4px;
+.k-button{
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color:$color;
+    border:1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out($color: #000000, $amount: 0.95);
+    & + & {
+        margin-left: 8px;
+    }
+    &:hover{
+
+    }
+    &:focus{
+        color:$blue;
+        border-color: $blue;
+        outline: none;
+    }
+    &::-moz-focus-inner{
+        border:0;
+    }
+
+
+}
 
 </style>
