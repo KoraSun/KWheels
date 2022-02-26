465254acedb54d5b167425f2af9a60153d7eef4e
kora <bettm9706@gmail.com>
2022-02-26 16:57:56 +0800

    支持size以及css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,12 +3,15 @@
       Button组件
       <h1>示例一</h1>
       <div>
-          <Button>默认按钮</Button>
-           <Button theme="button">
-                你好     
-          </Button>
+           <Button theme="button">默认主题 </Button>
           <Button theme="link">链接按钮</Button>
           <Button theme="text">文本按钮</Button>
+      </div>
+      <h1>示例二</h1>
+      <div>
+           <Button size="big"> 大大大</Button>
+          <Button size="normal">正正正</Button>
+          <Button size="small">小小小</Button>
       </div>
      
     </div>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,18 +1,33 @@
 <template>
     <button class="k-button"
-      :class="`k-theme-${theme}`"
+      :class="classes"
     >
         <slot />
     </button>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core'
     export default {
         props:{
             theme:{
                 type:String,
                 default:"button"
-            }
+            },
+            size:{
+                type:String,
+                default:"normal"
+            },
+        },
+        setup(props){
+            const {theme,size} =props
+            const classes=computed(()=>{
+                return {
+                    [`k-theme-${theme}`]:theme,
+                    [`k-size-${size}`]:size
+                };
+            })
+            return {classes}
         }
          
     }
@@ -70,9 +85,21 @@ $radius:4px;
         &:hover,&:focus{
             background: darken(white,5%);
         }
-
+    }
+    &.k-theme-button{
+        &.k-size-big{
+            font-size: 24px;
+            height: 48px;
+            padding: 0 16px;
+        }
+        &.k-size-small{
+            font-size: 12px;
+            height: 20px;
+            padding: 0 4px;
+        }
 
     }
+
 
 
 }
