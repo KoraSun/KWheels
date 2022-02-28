30de5ab0e66db27ed84933fa6946eb0b8de6c676
kora <bettm9706@gmail.com>
2022-02-28 14:41:04 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         Dialog组件
-
+        <h1>示例一</h1>
+        <Dialog></Dialog>
     </div>
 </template>
 
-<script>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
     export default {
+        components:{
+            Dialog
+        }
         
     }
 </script>
