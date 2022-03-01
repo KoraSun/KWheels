bc452f57d5c53885e07b8676020e879e68719a32
kora <bettm9706@gmail.com>
2022-03-01 20:37:49 +0800

    css实现内容切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-     <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-     <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="k-tabs">
+        <div class="k-tabs-nav">
+            <div class="k-tabs-nav-item" 
+                 @click="select(t)"
+                 :class="{selected:t===selected}"
+                 v-for="(t,index) in titles" :key="index">{{t}}
+            </div>
+        </div>
+        <div class="k-tabs-content">
+            <component class="k-tabs-content-item" 
+                       :class="{selected:c.props.title=== selected}"
+                       v-for="(c,index) in defaults" :is="c" :key="index"/>  
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
     export default {
+        props:{
+            selected:{
+                type:String
+            }     
+        },
         setup(props,context){
            const defaults=context.slots.default()
            defaults.forEach((tag)=>{
@@ -15,16 +30,56 @@ import Tab from './Tab.vue'
                    throw new Error('Tabs子标签必须是Tab')
                }
            })
+           const current = defaults.filter((tag)=>{
+               return tag.props.title === props.selected
+           })[0]
            const titles =defaults.map((tag)=>{
                return tag.props.title
            })
-           return {defaults,titles}
-
+           const select=(title:string)=>{
+               context.emit('update:selected',title)
+           }
+           return {defaults,titles,current,select}
         }
         
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$blue:#40a9ff;
+$color:#333;
+$border-color:#d9d9d9;
+
+.k-tabs{
+    &-nav{
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+        &-item{
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child{
+                margin-left: 0;
+
+            }
+            &.selected{
+                color: $blue;
+
+            }
+        }
+    }
+
+    &-content{
+        padding: 8px 0;
+        &-item{
+            display: none;
+            &.selected{
+                display: block;
+            }
+        }
+    }
+}
 
 </style>
