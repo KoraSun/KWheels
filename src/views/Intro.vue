84cb64c72d02dc2c1e0d95f28e546aeaf563da8a
kora <bettm9706@gmail.com>
2022-03-03 12:54:53 +0800

    引入github样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-介绍
-    </div>
+    <article class="markdown-body">
+        <h1>开始</h1>
+        <br>
+        欢迎使用KwheelsUI
+        <h2>介绍</h2>
+        <p>
+             PKwheelsUI是一个简洁的UI框架,基于vue3.0的开源UI组件库,使用`vue3.0``vite``typescript`搭建.
+        </p>
+       
+        
+
+    </article>
 </template>
 
 <script>
